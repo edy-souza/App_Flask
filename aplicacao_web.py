cf6fe7df9b00229941daa040b3e68da7c67cbcb3
Edivaldo Souza <edysouza363@gmail.com>
2025-11-23 15:40:02 -0300

    Start response

--- aplicacao_web.py
+++ aplicacao_web.py
@@ -8,11 +8,18 @@ def aplicacao(environ, start_response):
         {'nome' : 'Monitor IPS', 'valor' : '4520.00'},
         {'nome' : 'PC Game', 'valor' : '9.087.00'},
         ]
+    
+    linhas_html = ''
+    for produto in produtos:
+        linhas_html += f'<li>{produto['nome']} - R${produto['valor']}</li>'
+    
     start_response('200 OK', [('Content-type', 'text/html;charset=utf-8')])
     
     with open('index.html', 'r', encoding='utf-8') as file:
         html = file.read()
     
-    return [html.encode('utf-8')]
+    html_final = html.replace('{{PRODUTOS}}', linhas_html)
+    
+    return [html_final.encode('utf-8')]
 
 make_server('', 5000, aplicacao).serve_forever()

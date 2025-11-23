99d58d0bbaf4667f863af78192be1ea1767cedcf
Edivaldo Souza <edysouza363@gmail.com>
2025-11-23 15:22:16 -0300

    Index HTML

--- aplicacao_web.py
+++ aplicacao_web.py
@@ -1,10 +1,18 @@
 from wsgiref.simple_server import make_server # Desenvolver o Servidor 
 
 def aplicacao(environ, start_response):
-    
+    produtos = [
+        {'nome' : 'Notebook', 'valor' : '7499.99'},
+        {'nome' : 'Play Station 5', 'valor' : '5877.00'},
+        {'nome' : 'Tablet', 'valor' : '1399.99'},
+        {'nome' : 'Monitor IPS', 'valor' : '4520.00'},
+        {'nome' : 'PC Game', 'valor' : '9.087.00'},
+        ]
     start_response('200 OK', [('Content-type', 'text/html;charset=utf-8')])
-    html = ''
     
-    return [html]
+    with open('index.html', 'r', encoding='utf-8') as file:
+        html = file.read()
+    
+    return [html.encode('utf-8')]
 
 make_server('', 5000, aplicacao).serve_forever()
